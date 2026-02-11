e8c72962909f58e28325e329ece0683139047ca4
Valentin Perignon <valentin.perignon@infomaniak.com>
2026-02-11 11:22:10 +0100

    feat: Add UTType helper

--- Sources/InfomaniakFoundation/UTType+Utils.swift
+++ Sources/InfomaniakFoundation/UTType+Utils.swift
@@ -19,11 +19,12 @@
 import UniformTypeIdentifiers
 
 public extension UTType {
-    static let onlyOffice = UTType("org.oasis-open.opendocument.text")!
-    static let pages = UTType("com.apple.iwork.pages.sffpages")!
-    static let wordDoc = UTType("com.microsoft.word.doc")!
-    static let wordDocm = UTType(mimeType: "application/vnd.ms-word")!
-    static let wordDocx = UTType("org.openxmlformats.wordprocessingml.document")!
-
-    static let ics = UTType(mimeType: "application/ics")!
+    func conforms(to types: [UTType]) -> Bool {
+        for type in types {
+            if conforms(to: type) {
+                return true
+            }
+        }
+        return false
+    }
 }
